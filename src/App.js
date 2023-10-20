7ee37617f6217267793671af17ffe36c7dc3e667
I'm Groot <80740629+nlmupdy01@users.noreply.github.com>
2023-10-20 15:32:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CodeEditor from './Component/CodeEdidetor';
+
+
+
+function App() {
+  return (
+    <>
+
+  <CodeEditor />
+  
+   
+    </>
+  );
+}
+
+export default App;
